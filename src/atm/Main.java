ddc5690935963660ec3802117b9f5352acfaa29e
GIlbertSong <songzihao@live.cn>
2014-12-03 22:09:03 -0800

    main() print

--- src/atm/Main.java
+++ src/atm/Main.java
@@ -14,10 +14,14 @@ public class Main {
 			String[] op;
 			try {
 				op = br.readLine().split(" ");
-				if(op[0].equals("deposit"))
+				if(op[0].equals("deposit")){
 					atm.deposit(Integer.parseInt(op[1]));
-				else if(op[0].equals("withdraw"))
+					System.out.println("deposit " + op[1] + "; current balance " + atm.getBalance());
+				}	
+				else if(op[0].equals("withdraw")){
 					atm.withdraw(Integer.parseInt(op[1]));
+					System.out.println("withdraw " + op[1] + "; current balance " + atm.getBalance());
+				}
 				else
 					System.out.println("error command!");
 			} catch (Exception e) {
