9dafe6d10b7bdd6649c96fdea26ec0833148f704
myb800 <myb800@gmail.com>
2014-12-03 20:05:47 -0800

    helper

---
+++ src/atm/Helper.java
@@ -0,0 +1,27 @@
+package atm;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Helper {
+	public static String readFile(File file){
+		StringBuilder sb = new StringBuilder((int)file.length());
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String lineSeperator = "\n";
+		try{
+			while(sc.hasNextLine()){
+				sb.append(sc.nextLine() + lineSeperator);
+			}
+			return sb.toString();
+		}finally{
+			sc.close();
+		}
+	}
+}
