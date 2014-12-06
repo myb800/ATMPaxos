d0a7cbe42f3efd7caa2d1048942a395248077051
GIlbertSong <songzihao@live.cn>
2014-12-06 15:43:02 -0800

    init echo check

--- src/atm/Main.java
+++ src/atm/Main.java
@@ -1,6 +1,7 @@
 package atm;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -13,32 +14,36 @@ public class Main {
 		Constants.config(args[0]);
 		int idx = Integer.parseInt(args[1]);
 		ATM atm = new ATM(Constants.CLIENTS[idx].port, idx, Constants.CLIENTS[idx].recoveryPort,Constants.CLIENTS);
-		Socket[] echoSocket;
-		echoSocket = new Socket[5];
-		for(int j = 0; j < 5; j++){
-			Socket temp = new Socket(Constants.CLIENTS[j].address, Constants.CLIENTS[j].port);
-			echoSocket[j] = temp;
-			PrintWriter out = new PrintWriter(echoSocket[j].getOutputStream(), true);
-			out.println("Are you alive?");
-		}
-		PrintWriter out = new PrintWriter(echoSocket[idx].getOutputStream(), true);
-		BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket[idx].getInputStream()));
-
+		for(Node node:Constants.CLIENTS)
+			Client.send(node.port, "Are you alive?", node.address, null);
 		while(true){
-			if(in.readLine() != null)
-				out.println("Yes, I am alive!");
-			
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			String[] op;
 			try {
 				op = br.readLine().split(" ");
 				if(op[0].equals("deposit")){
+					for(Node node:Constants.CLIENTS)
+						Client.send(node.port, "Are you alive?", node.address, null);
+					initCheck serverActionDeposit = new initCheck();
+					new Thread(new Server(Constants.CLIENTS[idx].port, serverActionDeposit)).start();
+					if(serverActionDeposit.getreplyNum() > 2){
 					atm.deposit(Integer.parseInt(op[1]));
 					System.out.println("deposit " + op[1] + "; current balance " + atm.getBalance());
+					}
+					else
+						System.out.println("deposit FAILURE: cannot get majority reply.");
 				}	
 				else if(op[0].equals("withdraw")){
-					atm.withdraw(Integer.parseInt(op[1]));
-					System.out.println("withdraw " + op[1] + "; current balance " + atm.getBalance());
+					for(Node node:Constants.CLIENTS)
+						Client.send(node.port, "Are you alive?", node.address, null);
+					initCheck serverActionWithdraw = new initCheck();
+					new Thread(new Server(Constants.CLIENTS[idx].port, serverActionWithdraw)).start();
+					if(serverActionWithdraw.getreplyNum() > 2){
+						atm.withdraw(Integer.parseInt(op[1]));
+						System.out.println("withdraw " + op[1] + "; current balance " + atm.getBalance());
+					}
+					else
+						System.out.println("withdraw FAILURE: cannot get majority reply.");
 				}
 				else if(op[0].equals("balance")){
 					System.out.println("current balance " + atm.getBalance());
@@ -50,6 +55,24 @@ public class Main {
 				else
 					System.out.println("error command!");
 			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static class initCheck implements ServerAction{
+		private int replyNum = 0;
+		public int getreplyNum(){
+			return replyNum;
+		}
+		@Override
+		public void onRecv(String data, DataOutputStream replyStream) {
+			// TODO Auto-generated method stub
+			try {
+				replyStream.writeUTF("Yes, I am alive!");
+				replyNum++;
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
